e37c6cb4e949017d4e7d1a85a6b61597a95041fd
JorgecsJR <jorgecsjr22@gmail.com>
2025-08-28 17:57:56 -0300

    Update RootNavigation.js

--- app/navigation/RootNavigation.js
+++ app/navigation/RootNavigation.js
@@ -19,7 +19,7 @@ const RootNavigator = () => {
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{ headerShown: false }}
-        initialRouteName="Chat"
+        initialRouteName="EventDetails"
       >
         <Stack.Screen name="Login" component={Login}></Stack.Screen>
         <Stack.Screen name="EventDetails" component={EventDetails} />
